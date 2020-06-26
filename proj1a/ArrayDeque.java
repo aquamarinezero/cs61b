33c852e5d4d7789be847164625a935c69208625b
aquamarinezero <1556678196@qq.com>
2020-06-26 20:58:56 +0800

    fixed some mistakes

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -16,18 +16,21 @@ public class ArrayDeque<T> {
     }
     */
     private void resize(double RFACTOR){
-        T[] newItems = (T[])new Object[(int)RFACTOR * items.length];
+        T[] newItems = (T[])new Object[(int)(RFACTOR * items.length)];
         //需要扩充的情况肯定是，两个指针重合了，需要在两个指针中间进行扩充，分成两段
         System.arraycopy(items,0,newItems,0,nextLast);
         System.arraycopy(items,nextFirst+1,newItems,newItems.length-(items.length-nextFirst-1),items.length-nextFirst-1);
-        items = newItems;
         nextFirst = newItems.length-(items.length-nextFirst);
+        items = newItems;
     }
+
+    //JAVA里面取余的概念和C里面不一样，负数%正数还是负数，坑爹
+    //所有涉及到-1的操作，记得加上一个8，防止出现负数的情况
     public void addFirst(T item){
         if(nextFirst == nextLast)
             resize(2);
         items[nextFirst] = item;
-        nextFirst = (nextFirst - 1)%items.length;
+        nextFirst = (nextFirst - 1 + items.length)%items.length;
         size = size + 1;
     }
 
@@ -50,32 +53,60 @@ public class ArrayDeque<T> {
     public void printDeque(){
         if(nextLast>nextFirst+1){
             for(int i = nextFirst + 1; i < nextLast; i++)
-                System.out.println(items[i]+" ");}
+                System.out.print(items[i]+" ");}
         else if(nextLast < nextFirst){
             for(int i = nextFirst + 1;i < items.length;i++)
-                System.out.println(items[i]+" ");
+                System.out.print(items[i]+" ");
             for(int i = 0; i < nextLast;i++)
-                System.out.println(items[i]+" ");
+                System.out.print(items[i]+" ");
         }
     }
 
     public T removeFirst(){
+        if(size == 0)
+            return null;
         if(size <= 0.25 * items.length)
             resize(0.5);
         T result = items[(nextFirst + 1) % items.length];
         nextFirst = (nextFirst + 1) % items.length;
+        size = size - 1;
         return result;
     }
 
     public T removeLast(){
+        if(size == 0)
+            return null;
         if(size <= 0.25 * items.length)
             resize(0.5);
-        T result = items[(nextLast - 1) % items.length];
-        nextLast = (nextLast - 1) % items.length;
+        T result = items[(nextLast - 1 + items.length) % items.length];
+        nextLast = (nextLast - 1 + items.length) % items.length;
+        size = size - 1;
         return result;
     }
 
     public T get(int index){
         return items[(nextFirst+1+index)%items.length];
     }
+    /*
+    public static void main(String[] args){
+        ArrayDeque<String> ad = new ArrayDeque<String>();
+        ad.addLast("first");
+        ad.addFirst("irst");
+        ad.addFirst("irst");
+        ad.addFirst("irst");
+        ad.addFirst("irst");
+        ad.addFirst("irst");
+        ad.addFirst("irst");
+        ad.addFirst("second");
+        ad.removeLast();
+        ad.removeLast();
+        ad.removeLast();
+        ad.removeLast();
+        ad.removeLast();
+        ad.removeLast();
+        String a = ad.get(0);
+        System.out.println(a);
+        ad.printDeque();
+    }
+     */
 }
